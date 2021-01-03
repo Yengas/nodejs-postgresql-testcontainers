6126a1de07b327c1df6eb670a167e5a9da618b4c
Yengas <yigitcan@hotmail.com.tr>
2021-01-04 01:12:15 +0300

    add getActiveAdvertisementProducts implementation with sellerID and isActive check

--- src/product-intersection/product-intersection.repository.ts
+++ src/product-intersection/product-intersection.repository.ts
@@ -4,7 +4,33 @@ import {AdvertisementWithProducts} from '../advertisement/advertisement.reposito
 export class ProductIntersectionRepository {
   constructor(private readonly postgreSQLAdapter: PostgreSQLAdapter) {}
 
-  async getActiveAdvertisementProducts(): Promise<AdvertisementWithProducts[]> {
-    throw new Error('not implemented');
+  async getActiveAdvertisementProducts(
+    query: ProductIntersectionQuery
+  ): Promise<AdvertisementWithProducts[]> {
+    const sql = `
+      SELECT
+        a.id as "id", a.seller_id as "sellerID", a.is_active as "isActive", a.start_date_time as "startDateTime", a.end_date_time as "endDateTime",
+        json_agg(ap.product_id) as products
+      FROM advertisements as a
+      INNER JOIN advertisement_products ap ON ap.advertisement_id = a.id
+      WHERE
+        a.seller_id = $1
+        AND a.is_active = true
+      GROUP BY a.id
+    `;
+    const params = [query.sellerID];
+
+    const {
+      rows,
+    } = await this.postgreSQLAdapter.query<AdvertisementWithProducts>(
+      sql,
+      params
+    );
+
+    return rows;
   }
 }
+
+type ProductIntersectionQuery = {
+  sellerID: number;
+};
--- test/integration/product-intersection/product-intersection.repository.test.ts
+++ test/integration/product-intersection/product-intersection.repository.test.ts
@@ -1,15 +1,70 @@
 import {PostgreSQLAdapter} from '../../../src/postgresql/postgresql.adapter';
-import {AdvertisementRepository} from '../../../src/advertisement/advertisement.repository';
+import {
+  AdvertisementRepository,
+  AdvertisementWithProducts,
+} from '../../../src/advertisement/advertisement.repository';
 import {ProductIntersectionRepository} from '../../../src/product-intersection/product-intersection.repository';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const postgreSQLAdapter: PostgreSQLAdapter = (global as any).postgreSQLAdapter;
-const advertisementRepository = new AdvertisementRepository(postgreSQLAdapter);
+const adRepository = new AdvertisementRepository(postgreSQLAdapter);
 const repository = new ProductIntersectionRepository(postgreSQLAdapter);
 
 describe('ProductIntersectionRepository', () => {
+  const UUID_PREFIX = 'f4f4c9e3-a077-4f3c-bf73-9c54cb57ffa';
+
   beforeEach(async () => {
     await postgreSQLAdapter.query('DELETE FROM advertisements');
     await postgreSQLAdapter.query('DELETE FROM advertisement_products');
+  });
+
+  describe('.getActiveAdvertisementProducts()', () => {
+    const SELLER_ID = 968;
+    const OTHER_SELLER_ID = 73;
+
+    const BASE_ADVERTISEMENT: AdvertisementWithProducts = {
+      id: UUID_PREFIX + '0',
+      sellerID: OTHER_SELLER_ID,
+      isActive: false,
+      startDateTime: new Date(0),
+      endDateTime: new Date(0),
+      products: [],
+    };
+
+    it('should only return products for given sellers active products', async () => {
+      const advertisements: AdvertisementWithProducts[] = [
+        {
+          ...BASE_ADVERTISEMENT,
+          id: UUID_PREFIX + '1',
+          sellerID: SELLER_ID,
+          isActive: true,
+          products: [1, 2, 3],
+        },
+        {
+          ...BASE_ADVERTISEMENT,
+          id: UUID_PREFIX + '2',
+          sellerID: SELLER_ID,
+          isActive: false,
+          products: [4, 5, 6],
+        },
+        {
+          ...BASE_ADVERTISEMENT,
+          id: UUID_PREFIX + '3',
+          sellerID: OTHER_SELLER_ID,
+          isActive: true,
+          products: [7, 8, 9],
+        },
+      ];
+
+      const expectedResult = [advertisements[0]];
+
+      await adRepository.insertAdvertisementsWithProducts(advertisements);
+
+      const result = await repository.getActiveAdvertisementProducts({
+        sellerID: SELLER_ID,
+      });
+
+      expect(result).toEqual(expectedResult);
+    });
   });
 });
